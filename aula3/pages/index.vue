804427c84a605c1ca7a191583f7e1595bd26edb6
Mayara <mayara130605@gmail.com>
2022-05-03 16:42:48 -0300

    Adição do projeto Nuxt.js

--- aula3/pages/index.vue
+++ aula3/pages/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <Tutorial />
+<div class="container">
+  <h1>{{titulo}}</h1>
+  <ul>
+    <li :key="item.id" v-for = "item in pessoas">
+      {{item.nome}} - Idade: {{item.idade}} anos
+    </li>
+  </ul>
+  <button @click="digaOla"> Diga olá! </button>
+</div>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  data: function(){
+    return{
+      titulo: "Lista de pessoas e idades",
+      pessoas: [
+        {id: 1, nome:"Marcio", idade: "33"},
+        {id: 2, nome: "Diego", idade: "44"},
+        {id: 3, nome: "Luana", idade: "50"},
+        {id: 4, nome: "Tamara", idade: "23"},
+      ]
+    }
+  },
+  methods:{
+    digaOla: function(){
+      alert("Olá Mundo do Nuxt.js!")
+    }
+  }
 }
 </script>
+
+<style scoped>
+.container{
+  margin: 20px
+}
+</style>
